5e20dd9b45a92653a615d3afcd1c217a99a423e9
Nguyen Van Khanh Duy - 2280600480 <nguyenduy548433@gmail.com>
2026-04-10 11:27:47 +0700

    Last Update

--- backend/src/services/payment.service.js
+++ backend/src/services/payment.service.js
@@ -1,9 +1,9 @@
 import { PayOS } from '@payos/node';
 
 const payos = new PayOS({
-  clientId: process.env.PAYOS_CLIENT_ID || 'ae99da9f-dcc2-489f-83dd-579e88391fb3',
-  apiKey: process.env.PAYOS_API_KEY || '495ec2be-0ec3-4ebe-bfaf-a763d028d726',
-  checksumKey: process.env.PAYOS_CHECKSUM_KEY || 'bdb81320f0af89c65c0f08cae1912bf304ab98af98225157564806a31fbe7580'
+  clientId: process.env.PAYOS_CLIENT_ID,
+  apiKey: process.env.PAYOS_API_KEY,
+  checksumKey: process.env.PAYOS_CHECKSUM_KEY
 });
 
 export const createPayOSPaymentLink = async ({ orderCode, amount, bookingId, cancelUrl, returnUrl }) => {
